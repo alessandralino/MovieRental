680674d82c902e825f5fd5bedde70f9335d60844
Alessandra Lino <ale.lino.00@hotmail.com>
2025-12-27 14:15:52 +0000

    feat(payment): add payment service

---
+++ MovieRental/PaymentProviders/Service/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using MovieRental.PaymentProviders.Entities;
+
+namespace MovieRental.PaymentProviders.Service
+{
+    public interface IPaymentService
+    {
+        Task<PaymentResult> ProcessPaymentAsync(string paymentMethod, double amount);
+        IEnumerable<string> GetAvailablePaymentMethods();
+    }
+}
---
+++ MovieRental/PaymentProviders/Service/PaymentService.cs
@@ -0,0 +1,27 @@
+﻿using MovieRental.PaymentProviders.Entities;
+using MovieRental.PaymentProviders.Providers;
+
+namespace MovieRental.PaymentProviders.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly Dictionary<string, IPaymentProvider> _providers;
+        public PaymentService(IEnumerable<IPaymentProvider> providers)
+        {
+            _providers = providers.ToDictionary(
+                    p => p.ProviderName,
+                    p => p, 
+                    StringComparer.OrdinalIgnoreCase);
+        }   
+
+        public IEnumerable<string> GetAvailablePaymentMethods()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentResult> ProcessPaymentAsync(string paymentMethod, double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
