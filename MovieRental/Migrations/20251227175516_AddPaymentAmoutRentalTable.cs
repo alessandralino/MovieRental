eeba815740c6f38ce8ecc6027c4e7b755d15cf65
Alessandra Lino <ale.lino.00@hotmail.com>
2025-12-27 18:58:36 +0000

    chore(migrations): add customer entities and remove properties that were unnecessarily included

---
+++ MovieRental/Migrations/20251227175516_AddPaymentAmoutRentalTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentAmoutRentalTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PaymentAmout",
+                table: "Rentals",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentAmout",
+                table: "Rentals");
+        }
+    }
+}
